9bc39a46158acc05024d409d9d02035ef613ac22
KUGOM <yaeji0728@naver.com>
2024-08-16 02:41:22 +0900

    init commit

---
+++ src/test/java/com/sparta/springpersonalproject/SpringPersonalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springpersonalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPersonalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
