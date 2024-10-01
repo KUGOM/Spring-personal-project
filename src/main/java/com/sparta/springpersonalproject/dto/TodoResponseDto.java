0bad1b4fdb8b26c8954484dcffc3eae875ed49cd
KUGOM <yaeji0728@naver.com>
2024-10-01 10:28:56 +0900

    feat: 불필요한 파일 삭제
    불필요한 파일 삭제

--- src/main/java/com/sparta/springpersonalproject/dto/TodoResponseDto.java
+++ src/main/java/com/sparta/springpersonalproject/dto/TodoResponseDto.java
@@ -9,9 +9,9 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 @Getter
 public class TodoResponseDto {
-    private final long id;
+    private final Long id;
     private final String todo;
-    private final long managerId;
+    private final Long adminId;
     private final String adminName;
     private final String adminEmail;
     private final LocalDateTime createAt;
